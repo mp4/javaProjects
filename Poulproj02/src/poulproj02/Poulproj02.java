12f45c8cac521eec98f5ecc1a1bebd8201504eb8
Marsh Poulson IV <marsh@linux-7i17.site>
2013-09-04 22:02:29 -0600

    added mnue funtion

--- Poulproj02/src/poulproj02/Poulproj02.java
+++ Poulproj02/src/poulproj02/Poulproj02.java
@@ -1,9 +1,10 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * this is a menu driven program to test the methods c and rabbit
+ * by marsh poulson 9/04/2013
  */
 package poulproj02;
-
+import java.io.*;
+import java.util.Scanner;
 /**
  *
  * @author marsh poulson 9/02/2013
@@ -15,11 +16,19 @@ public class Poulproj02 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("welcome to the recurion testing program");
+        Scanner keyboard = new Scanner(System.in);
+        Menu(keyboard);
         c(4,2);
         rabbit(5);
     }
-    private static int stackedRabbitCalls = 0;
     
+    private static int stackedRabbitCalls = 0;
+    /*
+     * this is the spock problem which outputs each equation at each recursive
+     * call returns the number of ways to order a collection of n items in 
+     * groups of size k
+     */
     public static int c(int n, int k)
     {
         System.out.print("c(" + n +", " + k + ") = ");
@@ -40,6 +49,10 @@ public class Poulproj02 {
             return c(n-1, k-1) + c(n-1, k);
         }
     }
+    /*
+     * outputs to the user the begin and end of each recursive call for the 
+     * factorial funtion n is the n from n!
+     */
     public static int rabbit(int n)
     {
         int temp;
@@ -71,5 +84,46 @@ public class Poulproj02 {
             }
         System.out.println("Leave rabbit: n = "+n + "\tvalue = " + temp);
         return temp;
+    }
+    /*
+     * this function accepts a scanner which should already be directed to the 
+     * standard input device it will output a menu and then call the 
+     * appropraiate functions
+     */
+    public static void Menu(Scanner keyboard)
+    {
+        int choice;
+        System.out.println("menu");
+        System.out.println("1) c (spock dilema)");
+        System.out.println("2) rabbit (factorial)");
+        System.out.println("3) exit");
+        choice = getValidMenuInt(keyboard);
+    }
+    final static int MINMENUCHOICE = 1;
+    final static int MAXMENUCHOICE = 3;
+    /*
+     * this function will prompt the user to enter a valid menu integer
+     * and will return this value will reprompt as many times as necessary
+     * the parameter is an initialized instance of a scanner
+     */
+    public static int getValidMenuInt(Scanner keyboard)
+    {
+        try
+        {
+            int value;
+            System.out.println("please enter your choice:");
+            value = keyboard.nextInt();
+            if((value > MAXMENUCHOICE) || (value < MINMENUCHOICE))
+            {
+                return getValidMenuInt(keyboard);
+            }
+            //else
+            //base case
+            return value;
+        }
+        catch(Exception e)
+        {
+            return getValidMenuInt(keyboard);
+        }
     }
 }
