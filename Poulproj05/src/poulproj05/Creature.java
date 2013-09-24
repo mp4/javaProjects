d0445915f81972f3b7526912d51bee076a4c7cbb
Marsh Poulson IV <marsh@linux-7i17.site>
2013-09-23 20:01:12 -0600

    created files for project 05

---
+++ Poulproj05/src/poulproj05/Creature.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poulproj05;
+
+/**
+ *
+ * @author marsh
+ */
+public class Creature {
+    
+}
---
+++ Poulproj05/src/poulproj05/Maze.java
@@ -0,0 +1,13 @@
+/*
+ * the maze contains squares that are clear, walls, path to exit and visited 
+ * but not in final path
+ */
+package poulproj05;
+
+/**
+ *
+ * @author marsh
+ */
+public class Maze {
+    
+}
