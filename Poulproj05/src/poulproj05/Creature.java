85c502cbaf577f4c073ac1c0604291594df0a413
mp4 <therealmarshpoulson@gmail.com>
2013-09-24 11:05:02 -0600

    Update Creature.java

--- Poulproj05/src/poulproj05/Creature.java
+++ Poulproj05/src/poulproj05/Creature.java
@@ -9,5 +9,32 @@ package poulproj05;
  * @author marsh
  */
 public class Creature {
+    private int x_, y_;
+    private Maze maze_;
     
+    public void setMaze(Maze maze)
+    {
+        maze_ = maze;
+    }
+    
+    public void moveNorth()
+    {
+        
+    }
+    public void moveSouth()
+    {
+        
+    }
+    public void moveWest()
+    {
+        
+    }
+    public void moveEast()
+    {
+        
+    }
+    public void MarkPosition()
+    {
+        maze_[x][y] = "m";//set this to the proper letter
+    }
 }
