399f0dd5b13e951078ecdc409d8278adb25f9881
mp4 <therealmarshpoulson@gmail.com>
2013-09-27 10:15:30 -0600

    Update Poulproj05.java

--- Poulproj05/src/poulproj05/Poulproj05.java
+++ Poulproj05/src/poulproj05/Poulproj05.java
@@ -1,19 +1,41 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * this is the driver program to test the creature and maze classes
+ * marsh poulson 9/27/2013
  */
 package poulproj05;
 
-/**
- *
- * @author marsh
- */
+import java.util.Scanner;
+
+
 public class Poulproj05 {
 
+    public static Maze maze;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("welcome to the creature and maze driver program");
+        
+        getMazeFile();
+    }
+    
+    public static void getMazeFile()
+    {
+        try
+        {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("please enter a maze file:");
+            String file = scanner.nextLine();
+            System.out.println("filepath:" + file);
+            maze = new Maze(file);
+            maze.runCreature();
+            System.out.println(maze.toString());
+        }
+        catch(Exception e)
+        {
+            System.out.println("exception " + e.getMessage());
+            getMazeFile();
+        }
     }
 }
