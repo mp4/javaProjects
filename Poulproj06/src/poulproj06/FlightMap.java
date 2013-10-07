9da903ed1a949a3607fc0212a71043ae42e8fd89
mp4 <therealmarshpoulson@gmail.com>
2013-10-07 13:07:35 -0600

    Update FlightMap.java

--- Poulproj06/src/poulproj06/FlightMap.java
+++ Poulproj06/src/poulproj06/FlightMap.java
@@ -10,10 +10,10 @@ import java.util.LinkedList;
 public class FlightMap
 {
 	LinkedList<City>[] adjacencyList;
-	
+	private final int MAXSIZE = 20;
 	public FlightMap()
 	{
-		
+		adjacencyList = new LinkedList<City>[MAXSIZE];
 	}
 	
 	public void readInFlightMap(String cityFileName, String flightFileName)
@@ -23,11 +23,17 @@ public class FlightMap
 	
 	public void displayAllCities()
 	{
-		
+		for(int i=0; i< MAXSIZE; i++)
+		{
+			System.out.println(adjacencyList[i][0]);
+		}
 	}
 	public void displayAdjacentCities(City aCity)
 	{
-		
+		for(int i=1; i < adjacencyList[findIndex(aCity)].count ; i++)
+		{
+			adjacencyList[findIndex(aCity)][i];
+		}
 	}
 	public void markVisited(City aCity)
 	{
@@ -53,6 +59,10 @@ public class FlightMap
 	public boolean isPath(City origen, City destinationCity)
 	{
 		return false;
+	}
+	private int findIndex(City city)
+	{
+		
 	}
 }
 
