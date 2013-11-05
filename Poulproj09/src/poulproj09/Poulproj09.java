3f2c2fd96fa31c51bbbdafa60e6f0e782cbdd708
mp4 <therealmarshpoulson@gmail.com>
2013-11-05 10:57:27 -0700

    Update Poulproj09.java
    a bunch of comments and the greeting

--- Poulproj09/src/poulproj09/Poulproj09.java
+++ Poulproj09/src/poulproj09/Poulproj09.java
@@ -1,18 +1,24 @@
 /*
- * 
+ * this file contains the drvier program for project 09
+ * it is designed to be able to build a tree of different plants based 
+ * on questions from the user once it has enough information it will be able to relably 
+ * determine what kind of plant the user has by asking yes or no questions
+ * by marsh poulson 11/5/2013
  */
 package poulproj09;
 
 import java.util.Scanner;
 import BinaryTree.BinaryTree;
 /**
- *
- * @author marsh
+ * contains the methods needed to drive the decision tree the tree is 
+ * stored as the tree of knowledge
  */
 public class Poulproj09 {
 
     /**
-     * @param args the command line arguments
+     * initializes the tree of knowledge with its initial values or knowledge
+     * then greets the user and then calls the menu which will run until the user asks
+     * to quit
      */
     public static void main(String[] args) {
         // TODO code application logic here
@@ -20,7 +26,7 @@ public class Poulproj09 {
         treeOfKnowledge = new BinaryTree<>("is it a monocot?",
                 new BinaryTree("green bean plant"), 
                 new BinaryTree<>("corn plant"));
-        
+        System.out.println("welcome to the plant guesser");
         menu(keyboard);
     }
     
@@ -48,7 +54,8 @@ public class Poulproj09 {
        /*
      * this function accepts a scanner which should already be directed to the 
      * standard input device it will output a menu and then call the 
-     * appropraiate functions including itself
+     * appropraiate functions including itself drives the think of a plant dialogue 
+     * and repeats itself as neccessary
      */
     public static void menu(Scanner keyboard)
     {
