311758d1494b859f7f02689b3d9798870e655f2d
marsh poulson iv <marsh@linux-bz39.site>
2013-11-28 13:34:34 -0700

    work in progress in driver and class

--- Poulproj11/src/poulproj11/Poulproj11.java
+++ Poulproj11/src/poulproj11/Poulproj11.java
@@ -6,17 +6,105 @@
 
 package poulproj11;
 
+import java.util.Scanner;
+
 /**
  *
  * @author marsh
  */
 public class Poulproj11 {
 
+    static SymbolTable table;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        table = new SymbolTable();
+        Scanner keyboard = new Scanner(System.in);
+        menu(keyboard);
         // TODO code application logic here
     }
-    
+        /*
+     * this function accepts a scanner which should already be directed to the 
+     * standard input device it will output a menu and then call the 
+     * appropraiate functions including itself
+     */
+    public static void menu(Scanner keyboard)
+    {
+        int choice;
+        System.out.println("\nmenu");
+        System.out.println("1) add");
+        System.out.println("2) remove");
+        System.out.println("3) is empty");
+        System.out.println("4) exit");
+        choice = getValidMenuInt(keyboard);
+        System.out.println("");
+        
+        switch(choice)
+        {
+            case 1:
+                
+                menu(keyboard);
+                break;
+            case 2:
+                try
+                {
+                    
+                }
+                catch(Exception e)
+                {
+                    System.out.println(e);
+                }
+                menu(keyboard);
+                break;
+            case 3:
+                menu(keyboard);
+                break;
+            case 4:
+                //fallout of the menu
+                break;
+        } 
+    }
+    final static int MINMENUCHOICE = 1;
+    final static int MAXMENUCHOICE = 5;
+    /*
+     * this function will prompt the user to enter a valid menu integer
+     * and will return this value will reprompt as many times as necessary
+     * the parameter is an initialized instance of a scanner
+     */
+    public static int getValidMenuInt(Scanner keyboard)
+    {
+        try
+        {
+            int value;
+            System.out.println("please enter your choice:");
+            value = keyboard.nextInt();
+            keyboard.nextLine();//purges \n from the stream
+            if((value > MAXMENUCHOICE) || (value < MINMENUCHOICE))
+            {
+                return getValidMenuInt(keyboard);
+            }
+            //else
+            //base case
+            return value;
+        }
+        catch(Exception e)
+        {
+            return getValidMenuInt(keyboard);
+        }
+    }
+    public void AddToTable(Scanner keyboard)
+    {
+        try
+        {
+            System.out.println("please enter a vaild java identifier:");
+            String identifier = keyboard.nextLine();
+            table.Add(identifier);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            AddToTable(keyboard);
+        }
+    }
 }
--- Poulproj11/src/poulproj11/SymbolTable.java
+++ Poulproj11/src/poulproj11/SymbolTable.java
@@ -3,78 +3,149 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package poulproj11;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
+
 /**
  *
  * @author marsh
  */
 public class SymbolTable {
-    static final int EMPTY_ALWAYS=0, FULL=1, EMPTY_NOW=2;
-    class Node{
+
+    static final int EMPTY_ALWAYS = 0, FULL = 1, EMPTY_NOW = 2, SIZE = 1009, MAX_STEP = 14;
+
+    class Node {
+
         String data;
         int status;
+
+        public Node() {
+            status = EMPTY_ALWAYS;
+        }
+
+        public Node(String item) {
+            data = item;
+            status = FULL;
+        }
     }
-    ArrayList<String> symbols;
-    public SymbolTable(int size)
-    {
-       symbols = new ArrayList<>();
+    ArrayList<Node> symbols;
+
+    public SymbolTable() {
+        symbols = new ArrayList<>(SIZE);
+    }
+
+    private int hash1(String data) {
+        return data.hashCode() % SIZE;
+    }
+
+    private int hash2(String data) {
+        return MAX_STEP - (data.hashCode() % (MAX_STEP - 1));
     }
     /*
      * returns wheter or not a string is a valid java identifier
      */
-    static boolean validJavaIdentifier(String identifier)
-    {
+
+    static boolean validJavaIdentifier(String identifier) {
         //exclude reserved words
-        if(identifier.equals("abstract") || identifier.equals("continue") ||
-                identifier.equals("for")|| 	identifier.equals("new") ||
-                identifier.equals("switch")||identifier.equals("assert")||
-                identifier.equals("default")||	identifier.equals("goto")||
-                identifier.equals("package")||
-                identifier.equals("synchronized")||
-                identifier.equals("boolean")||
-                identifier.equals("do")|| 	identifier.equals("if")||
-                identifier.equals("private")|| 	identifier.equals("this")||
-                identifier.equals("break")|| 	identifier.equals("double")||
-                identifier.equals("implements")||
-                identifier.equals("protected")||
-                identifier.equals("throw")||
-                identifier.equals("byte")|| 	identifier.equals("else")||
-                identifier.equals("import") ||	identifier.equals("public")||
-                identifier.equals("throws")|| identifier.equals("case")||
-                identifier.equals("enum") || identifier.equals("instanceof")||
-                identifier.equals("return") || identifier.equals("transient")||
-                identifier.equals("catch")|| 	identifier.equals("extends")||
-                identifier.equals("int") || 	identifier.equals("short")||
-                identifier.equals("try")||
-                identifier.equals("char")|| 	identifier.equals("final")|| 	
-                identifier.equals("interface")|| identifier.equals("static")|| 	
-                identifier.equals("void")||
-                identifier.equals("class")|| 	identifier.equals("finally")||
-                identifier.equals("long")|| 	identifier.equals("strictfp")||
-                identifier.equals("volatile")||
-                identifier.equals("const")|| 	identifier.equals("float")||
-                identifier.equals("native")|| 	identifier.equals("super")||
-                identifier.equals("while") || identifier.isEmpty())
-        {
+        if (identifier.equals("abstract") || identifier.equals("continue")
+                || identifier.equals("for") || identifier.equals("new")
+                || identifier.equals("switch") || identifier.equals("assert")
+                || identifier.equals("default") || identifier.equals("goto")
+                || identifier.equals("package")
+                || identifier.equals("synchronized")
+                || identifier.equals("boolean")
+                || identifier.equals("do") || identifier.equals("if")
+                || identifier.equals("private") || identifier.equals("this")
+                || identifier.equals("break") || identifier.equals("double")
+                || identifier.equals("implements")
+                || identifier.equals("protected")
+                || identifier.equals("throw")
+                || identifier.equals("byte") || identifier.equals("else")
+                || identifier.equals("import") || identifier.equals("public")
+                || identifier.equals("throws") || identifier.equals("case")
+                || identifier.equals("enum") || identifier.equals("instanceof")
+                || identifier.equals("return") || identifier.equals("transient")
+                || identifier.equals("catch") || identifier.equals("extends")
+                || identifier.equals("int") || identifier.equals("short")
+                || identifier.equals("try")
+                || identifier.equals("char") || identifier.equals("final")
+                || identifier.equals("interface") || identifier.equals("static")
+                || identifier.equals("void")
+                || identifier.equals("class") || identifier.equals("finally")
+                || identifier.equals("long") || identifier.equals("strictfp")
+                || identifier.equals("volatile")
+                || identifier.equals("const") || identifier.equals("float")
+                || identifier.equals("native") || identifier.equals("super")
+                || identifier.equals("while") || identifier.isEmpty()) {
             return false;
         }
         //checck start of string
-        if(!Character.isJavaIdentifierStart(identifier.charAt(0)))
-        {
+        if (!Character.isJavaIdentifierStart(identifier.charAt(0))) {
             return false;
         }
         //review the rest of the string
-        for(int i=1; i < identifier.length(); i++)
-        {
-            if(!Character.isJavaIdentifierPart(identifier.charAt(i)))
-            {
+        for (int i = 1; i < identifier.length(); i++) {
+            if (!Character.isJavaIdentifierPart(identifier.charAt(i))) {
                 return false;
             }
         }
         return true;
+    }
+    /*
+     * adds an item to the symbol table if it does not already exist throws an 
+     * exception if the symbol table is full also throws an exception if the
+     * item is not a valid java identifier
+     */
+
+    public void Add(String item) throws IllegalAccessException {
+
+        if(!validJavaIdentifier(item))
+        {
+            throw new IllegalArgumentException(
+                    "item must be a valid java identifier");
+        }
+        switch (symbols.get(hash1(item)).status) {
+            case EMPTY_ALWAYS:
+                symbols.add(hash1(item), new Node(item));
+                break;
+            case EMPTY_NOW:
+                symbols.add(hash1(item), new Node(item));
+                break;
+            case FULL:
+                //march with double hashing until it reaches an empty location
+                int nextStatus = FULL;
+                int index = hash1(item);
+                if (symbols.get(index).data.equals(item)) {
+                    return;
+                }
+                while (nextStatus == FULL) {
+                    index += hash2(item);
+                    index = index % SIZE;
+                    nextStatus = symbols.get(index).status;
+                    if (index == hash1(item)) {
+                        throw new ArrayStoreException("hash table is full");
+                    }
+                    if (symbols.get(index).data.equals(item)) {
+                        return;
+                    }
+                }
+                symbols.add(index, new Node(item));
+                break;
+            default:
+                throw new IllegalAccessException("the status of the location is not valid");
+        }
+
+    }
+    /*
+     * outputs all of the symbols currently in the symbol table to the user
+     */
+
+    public void outputAllSymbols() {
+        for (int i = 0; i < SIZE; i++) {
+            if (symbols.get(i).status == FULL) {
+                System.out.println(symbols.get(i).data);
+            }
+        }
     }
 }
